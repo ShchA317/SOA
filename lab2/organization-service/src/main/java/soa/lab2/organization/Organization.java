e5d3d7a5d1f72a64a1e086d68f09ad55ececc328
Alexander Shcherbakov <Neegroom@yandex.ru>
2024-10-25 08:10:01 +0300

    lab2: validation organization

--- lab2/organization-service/src/main/java/soa/lab2/organization/Organization.java
+++ lab2/organization-service/src/main/java/soa/lab2/organization/Organization.java
@@ -1,60 +1,35 @@
 package soa.lab2.organization;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
 public class Organization {
+
+    @NotNull(message = "Organization cannot be null")
     private long id;
+
+    @NotNull(message = "Full name cannot be null")
+    @Size(min = 2, max = 100, message = "Full name must be between 2 and 100 characters")
     private String fullName;
+
+    @NotNull(message = "Official address cannot be null")
+    @Size(min = 5, max = 200, message = "Address must be between 5 and 200 characters")
     private String officialAddress;
+
+    @Min(value = 1, message = "Employees count must be greater than or equal to 1")
     private int employeesCount;
+
+    @Min(value = 1, message = "Annual turnover must be greater than or equal to 1")
     private int annualTurnover;
+
+    @NotNull(message = "Creation date cannot be null")
+    @PastOrPresent(message = "Creation date must be in the past or present")
     private String creationDate;
-
-    // Геттеры и сеттеры
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getOfficialAddress() {
-        return officialAddress;
-    }
-
-    public void setOfficialAddress(String officialAddress) {
-        this.officialAddress = officialAddress;
-    }
-
-    public int getEmployeesCount() {
-        return employeesCount;
-    }
-
-    public void setEmployeesCount(int employeesCount) {
-        this.employeesCount = employeesCount;
-    }
-
-    public int getAnnualTurnover() {
-        return annualTurnover;
-    }
-
-    public void setAnnualTurnover(int annualTurnover) {
-        this.annualTurnover = annualTurnover;
-    }
-
-    public String getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(String creationDate) {
-        this.creationDate = creationDate;
-    }
 }
