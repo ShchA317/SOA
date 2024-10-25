7e4df0ee30712c798b16bf7854a9e0da3366b5ef
Alexander Shcherbakov <Neegroom@yandex.ru>
2024-10-25 07:25:53 +0300

    lab2: fix build and start

--- lab2/org-manager/src/main/java/soa/lab2/orgmanager/client/OrganizationsFeignClient.java
+++ lab2/org-manager/src/main/java/soa/lab2/orgmanager/client/OrganizationsFeignClient.java
@@ -3,7 +3,7 @@ package soa.lab2.orgmanager.client;
 import com.example.api.OrganizationsApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "organizationsClient", url = "${api.url}/organizations")
+@FeignClient(name = "organizationsClient", url = "api/organizations")
 public interface OrganizationsFeignClient extends OrganizationsApi {
 
 }
