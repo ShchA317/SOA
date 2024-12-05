52e05e978a4502bda929a4fb36f46b037a5a1f97
kkkooolllyyyaaa <kolya_cypandin@mail.ru>
2024-12-05 17:55:23 +0300

    api-gateway

---
+++ lab3/api-gateway/src/main/java/ru/negroom/service_discovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package ru.negroom.service_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
+
+}
